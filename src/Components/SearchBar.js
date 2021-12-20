b63f2d6b23f56526859698f6b91b30b59c7c71e6
Kamil Tkacz <kamiltkacz@gmail.com>
2021-12-20 18:43:51 -0300

    console msg

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -23,6 +23,7 @@ function SearchBar({ placeholder }) {
     setWordEntered(searchWord);
     const newFilter = pokemons.filter((value) => {
       return value.name.toLowerCase().includes(searchWord.toLowerCase());
+
     });
 
     if (searchWord === "") {
@@ -32,6 +33,8 @@ function SearchBar({ placeholder }) {
     }
     console.log(newFilter);
   };
+
+  let checkConsole = () =>  alert("Check the console :)");
 
   return (
     <div className="search-div">
@@ -60,13 +63,14 @@ function SearchBar({ placeholder }) {
               zIndex: 200,
               cursor: "pointer",
             }}
+            onClick={checkConsole}
           >
             <path
               fill="currentColor"
               d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
             ></path>
           </svg>
-          <div className="svg-div"></div>
+          <div className="svg-div" onClick={checkConsole}></div>
         </div>
       </div>
     </div>
