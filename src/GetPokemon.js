e84e6ce93980a856e2d6154fff7f714fac9d5a9c
Kamil Tkacz <kamiltkacz@gmail.com>
2022-04-27 10:16:19 -0300

    useState directly

--- src/GetPokemon.js
+++ src/GetPokemon.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 
 export default function Pokes() {
-  const [pokemonData, setPokemonData] = React.useState({});
+  const [pokemonData, setPokemonData] = useState({});
 
   useEffect(() => {
     fetch(
